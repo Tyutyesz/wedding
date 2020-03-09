25ede10110c883510ad0e1d5caf70edf133a3bf2
Matyi <matyas.csorba@melkweg.hu>
2020-03-09 17:29:30 +0100

    navigation finished, hero started

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,1 +1,26 @@
+import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../styles/main.scss';
+
+const Core = {
+    init() {
+        this.navigations();
+    },
+    navigations() {
+        const $btn = document.querySelector('.mobile-btn');
+        const $menu = document.querySelector('.menu');
+        let opened = false;
+        const toggleNav = () => {
+            if (!opened) {
+                $menu.classList.add('opened');
+            } else {
+                $menu.classList.remove('opened');
+            }
+            opened = !opened;
+        };
+        $btn.addEventListener('click', toggleNav);
+    },
+};
+
+window.addEventListener('load', () => {
+    Core.init();
+});
